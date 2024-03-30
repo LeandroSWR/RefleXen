2921f08cc1da493848860f57f35143f5a2742a3c
LeandroSWR <leandro-xy@hotmail.com>
2024-03-30 15:44:59 +0000

    first commit

---
+++ src/RefleXen.cpp
@@ -0,0 +1,12 @@
+﻿// RefleXen.cpp : Defines the entry point for the application.
+//
+
+#include "RefleXen.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
